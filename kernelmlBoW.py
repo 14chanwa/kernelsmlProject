d235d7983cd64faedc7f5380ef5a40e7b14f56d5
imkemayer <imke_mayer@Imkes-MacBook-Air.local>
2018-02-19 17:23:10 +0100

    Add some additional tests for Log Regression and SVM

--- kernelmlBoW.py
+++ kernelmlBoW.py
@@ -39,19 +39,90 @@ def split_train_test(X,y,prop=0.9):
     return X_train, y_train, X_test, y_test
 
 #%%
-Xtr, Ytr, Xte, Yte = split_train_test(Xtr0,Ytr0_labels,prop=0.8) 
-
+Xtr, Ytr, Xte, Yte = split_train_test(Xtr0,Ytr0_labels,prop=0.5) 
   
 n = Xtr.shape[0]
-lambds = np.logspace(np.log10(0.1), np.log10(100), 20)
+lambds = np.logspace(np.log10(0.1), np.log10(100), 10)
 acc = np.zeros(len(lambds))
+logistic_regression = LogisticRegression(center=False) 
 for i in range(len(lambds)):
     # LocisticRegression(center=True) would probably be better but problem with CenteredKernel
-    logistic_regression = LogisticRegression() 
-    logistic_regression.train(Xtr, Ytr, n, lambds[i])
+    if i == 0:
+        logistic_regression.train(Xtr, Ytr, n, lambds[i])
+    else:
+        logistic_regression.train(Xtr, Ytr, n, lambds[i],logistic_regression.K)
 
     f = logistic_regression.predict(Xte, Xte.shape[0])
 
     tmp = Yte == np.sign(f)
     acc[i] = np.sum(tmp) / np.size(tmp)
     print("Accuracy on test with linear kernel logistic regression:", acc[i])
+    
+    
+#%%
+Xtr, Ytr, Xte, Yte = split_train_test(Xtr0,Ytr0_labels,prop=0.8) 
+
+n = Xtr.shape[0]
+
+# Try different values for the regularizer lambda
+# Remark: when using sklearn a good value of C = 1/(2*n*lambda) was C = 0.01
+# So we expect to find that lambda ~ 30 is a good value here
+
+lambds = np.logspace(np.log10(0.1), np.log10(100), 10)
+acc = np.zeros(len(lambds))
+
+for i in range(len(lambds)):
+    svm = SVM(Gaussian_kernel(38.0)) 
+    svm.train(Xtr, Ytr, n, lambds[i])
+
+    f = svm.predict(Xte, Xte.shape[0])
+
+    tmp = Yte == np.sign(f)
+    acc[i] = np.sum(tmp) / np.size(tmp)
+    print("Accuracy on test with gaussian kernel SVM:", acc[i])
+    
+#%%
+Xtr, Ytr, Xte, Yte = split_train_test(Xtr0,Ytr0_labels,prop=0.8) 
+
+n = Xtr.shape[0]
+# Try different values for the kernel parameter gamma
+
+# First on coarse grid
+#gammas = np.logspace(np.log10(10), np.log10(200), 10)
+
+# Second on finer grid
+gammas = np.linspace(110, 160, 20)
+acc = np.zeros(len(gammas))
+
+for i in range(len(gammas)):
+    svm = SVM(Gaussian_kernel(gammas[i])) 
+    svm.train(Xtr, Ytr, n, 30)
+
+    f = svm.predict(Xte, Xte.shape[0])
+
+    tmp = Yte == np.sign(f)
+    acc[i] = np.sum(tmp) / np.size(tmp)
+    print("Accuracy on test with gaussian kernel (gamma = ", gammas[i], ") SVM:",acc[i])
+  
+#%%
+
+nb_trials = 10
+avg_acc = 0.0
+for k in range(nb_trials):
+    N = Xtr0.shape[0]
+    permut = np.random.permutation(N)
+    Xtr, Ytr, Xte, Yte = split_train_test(Xtr0[permut,],Ytr0_labels[permut],prop=0.8) 
+    n = Xtr.shape[0]
+
+    gamma = 120
+    lambd = 30
+
+    svm = SVM(Gaussian_kernel(gamma)) 
+    svm.train(Xtr, Ytr, n, 30)
+    f = svm.predict(Xte, Xte.shape[0])
+    tmp = Yte == np.sign(f)
+    acc = np.sum(tmp) / np.size(tmp)
+    avg_acc += acc
+    print("Accuracy on test with gaussian kernel (gamma = ", gamma, ") SVM:",acc)
+
+print(avg_acc/nb_trials)
